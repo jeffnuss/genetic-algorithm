c6fca5503ffa0237517603199b7e1571a1cc641d
Jeff Nuss <jeffnuss@hotmail.com>
2012-03-14 15:53:39 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace graves {
+    class Program {
+
+        static public Dictionary<int, cemetery> cems = new Dictionary<int, cemetery>();
+
+        static void Main(string[] args) {
+
+            //A friendly welcoming message
+            Console.WriteLine("Welcome to the Cemetery Distance Calculator.  Reading the data");
+
+            //Reading in the data
+            readData();
+
+            //Pausing after running
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Reading in the data from the two csv files
+        /// </summary>
+        public static void readData() {
+            //==========================================
+            //      Reading in the cemetery data
+            //==========================================
+            TextFieldParser parser = new TextFieldParser(@"cems.csv");
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            int i = 0;
+
+            //Looping through each line
+            while (!parser.EndOfData) {
+
+                //Reading the line
+                string[] fields = parser.ReadFields();
+
+                //Skipping the first row
+                i++; if (i == 1) continue;
+
+                //Creating a new cemetery object
+                cemetery c = new cemetery();
+                c.cemetery_id = int.Parse(fields[0]);
+                c.cemetery_name = fields[1];
+                c.lat = float.Parse(fields[2]);
+                c.lon = float.Parse(fields[3]);
+                cems.Add(c.cemetery_id, c);
+
+            }
+
+            //Closing the parser
+            parser.Close();
+
+            //==========================================
+            //      Reading in the temperature data
+            //==========================================
+            parser = new TextFieldParser(@"temps.csv");
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            i = 0;
+
+            //Looping through each line
+            while (!parser.EndOfData) {
+
+                //Reading the line
+                string[] fields = parser.ReadFields();
+
+                //Skipping the first row
+                i++; if (i == 1) continue;
+
+                //Creating a new cemetery object
+                cemetery c1 = cems[int.Parse(fields[0])];
+
+                //Reading in the temperature data
+                List<int> temps = new List<int>();
+                int j = 0;
+                foreach (string f in fields) {
+                    j++; if (j == 1) continue;
+                    temps.Add(int.Parse(f));
+                }
+                c1.temps = temps;
+
+            }
+
+        }
+      
+        
+
+
+    }
+}
---
+++ cemetery.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace graves {
+    class cemetery {
+        public int cemetery_id;
+        public string cemetery_name;
+        public float lat;
+        public float lon;
+        public List<int> temps;
+
+        public double distanceTo(int cemetery_id) {
+            cemetery c = Program.cems[cemetery_id];
+
+            return distance(this.lat, this.lon, c.lat, c.lon, 'M');
+        }
+
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        //:::  Definitions:                                                           :::
+        //:::    South latitudes are negative, east longitudes are positive           :::
+        //:::                                                                         :::
+        //:::  Passed to function:                                                    :::
+        //:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+        //:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+        //:::    unit = the unit you desire for results                               :::
+        //:::           where: 'M' is statute miles                                   :::
+        //:::                  'K' is kilometers (default)                            :::
+        //:::                  'N' is nautical miles                                  :::
+        //:::                                                                         :::
+        //:::  United States ZIP Code/ Canadian Postal Code databases with latitude   :::
+        //:::  & longitude are available at http://www.zipcodeworld.com               :::
+        //:::                                                                         :::S
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        static private double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
+            double theta = lon1 - lon2;
+            double dist = Math.Sin(deg2rad(lat1)) * Math.Sin(deg2rad(lat2)) + Math.Cos(deg2rad(lat1)) * Math.Cos(deg2rad(lat2)) * Math.Cos(deg2rad(theta));
+            dist = Math.Acos(dist);
+            dist = rad2deg(dist);
+            dist = dist * 60 * 1.1515;
+            if (unit == 'K') {
+                dist = dist * 1.609344;
+            } else if (unit == 'N') {
+                dist = dist * 0.8684;
+            }
+            return (dist);
+        }
+
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        //::  This function converts decimal degrees to radians             :::
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        static private double deg2rad(double deg) {
+            return (deg * Math.PI / 180.0);
+        }
+
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        //::  This function converts radians to decimal degrees             :::
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        static private double rad2deg(double rad) {
+            return (rad / Math.PI * 180.0);
+        }
+    }
+}
